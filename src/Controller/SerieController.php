d1c6397d38a133149ca22703c202c01952fd734f
therbron2023 <tom.herbron@proton.me>
2023-05-10 16:56:40 +0200

    Installation Symfony - Project series / entity + dbconfig

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
 
 #[Route('/series', name: 'serie_')]
 class SerieController extends AbstractController
@@ -25,6 +27,23 @@ class SerieController extends AbstractController
     #[Route('/add', name: 'add')]
     public function index(): Response
     {
+        $show = new Serie();
+        $show
+            -> setName("Utopia")
+            -> setBackdrop("backdrop.png")
+            -> setDateCreated(new \DateTime())
+            -> setGenres("Thriller/Drama")
+            -> setFirstAirDate(new \DateTime("-2 year"))
+            -> setLastAirDate(new \DateTime("-2 month"))
+            -> setPopularity(500)
+            -> setPoster("poster.png")
+            -> setStatus("Canceled")
+            -> setTmdbId(123456)
+            -> setVote(5);
+
+
+
+
         return $this->render('serie/add.html.twig');
     }
 
